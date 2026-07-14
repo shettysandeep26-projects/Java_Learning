1a3cb440c50f4bd600c2a9aa0e0ddff6a97b7082
Sandeep <sandeepa.shetty@buddyloan.com>
2026-07-14 16:02:36 +0530

    Added

--- src/main/java/org/example/Learn.java
+++ src/main/java/org/example/Learn.java
@@ -58,7 +58,7 @@ public class Learn {
         }
     }
 
-    public void set(){
+    public void set() {
         Set<String> set = new HashSet<>();
         set.add("a");
         set.add("b");
@@ -66,13 +66,91 @@ public class Learn {
         System.out.println(set);
     }
 
-    public void work() {
+    public void arrayList() {
         List<String> list = new ArrayList<>();
         list.add("a");
         list.add("b");
-        list.add("b");
         System.out.println(list.add("c"));
         System.out.println(list);
+    }
+
+    public void duplicateInArray() {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 1, 2, 3};
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            if (!set.add(i)) {
+                System.out.println("Duplicate element: " + i);
+            }
+        }
+    }
+
+    public void searchElementInArray() {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int searchElement = 1;
+        boolean found = false;
+        for (int num : arr) {
+            if (num == searchElement) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println(searchElement + " is found in the array.");
+        } else {
+            System.out.println(searchElement + " is not found in the array.");
+        }
+    }
+
+    public void largestElementInArray() {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        System.out.println("Largest element in the array is: " + largest);
+    }
+
+    public void sortArray() {
+        int[] arr = {10, 6, 1, 5, 7, 9, 8};
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+
+    public void secondLargestElementInArray() {
+        int[] arr = new int[]{10, 6, 1, 5, 7, 9, 8};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        System.out.println("Second largest element in the array is: " + secondLargest);
+
+//        {
+//            int[] arr = new int[]{10, 6, 1, 5, 7, 9, 8};
+//            Arrays.sort(arr);
+//            System.out.println("Sorted array: " + Arrays.toString(arr));
+//            System.out.println("Second largest element in the array is: " + arr[arr.length - 2]);
+//        }
+    }
+
+    public void work() {
 
     }
 
